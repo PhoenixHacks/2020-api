92e96f397d841abeed39105c771888bd9decec97
Justin McGettigan <jwmcgettigan@gmail.com>
2020-01-22 23:16:47 -0500

    Complete refactor of server.js; much cleaner now

--- server.js
+++ server.js
@@ -1,111 +1,122 @@
 // server.js
-
-const discord = require('discord.js');
-
-//const { channel_id, bot_token, port } = require('./config');
 const CHANNEL_ID = process.env.CHANNEL_ID || '666881141126725642';
 const BOT_TOKEN = process.env.BOT_TOKEN || 'NjY2ODUxNTEzNzI0OTYwNzg5.Xh_SmA.SjjrZQi7q_eHfB-E9wke3hCeIw8';
 const PORT = process.env.PORT || 3001;
+let announcements = [], channel;
 
-let channel;
-let announcements = [];
-const INDEX = 'index.html';
+const deletedMessageFilter = () => ((message) => !message.deleted);
+const isEdited = (message) => message.edits.length > 1;
+const getMostRecentEdit = (message) => message.edits.shift();
 
-const server = require('express')()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+class DiscordBot {
+  constructor(ss) {
+    const bot = new (require('discord.js')).Client({
+      autorun: true,
+      token: BOT_TOKEN
+    });
 
-const bot = new discord.Client({
-  autorun: true,
-  token: BOT_TOKEN
-});
+    this.bot = bot;
+    this.ss = ss;
+  }
 
-const io = require('socket.io')(server, {
-  path: '/announcements',
-  serveClient: false,
-});
+  onReady = () => {
+    this.bot.on('ready', () => {
+      console.log('Logged in as %s - %s\n', this.bot.user.tag, this.bot.user.id);
+      channel = this.bot.channels.get(CHANNEL_ID);
+  
+      // Get Announcements
+      channel.fetchMessages({ limit: 20 })
+      .then(messages => {
+        messages = messages.filter(deletedMessageFilter());
+        announcements = messages.map(m => ({
+          "id": m.id,
+          "time": m.createdAt,
+          "message": (isEdited(m) ? getMostRecentEdit(m) : m.content)
+        })).reverse();
+      }).catch(console.error);
 
-const deletedMessageFilter = () => {
-  return (message) => {
-    return !message.deleted;
-  };
-};
+      this.ss.onConnection()
+  
+      this.onMessage();
+      this.onMessageUpdate();
+      this.onMessageDelete();
+    });
+  }
 
-const isEdited = (message) => {
-  return message.edits.length > 1;
-}
+  onMessage = () => {
+    // When a new message is created, add it to the announcements
+    this.bot.on('message', m => {
+      if (m.channel.id === CHANNEL_ID) {
+        announcements.push({
+          "id": m.id,
+          "time": m.createdAt, 
+          "message": m.content
+        });
+        this.ss.emit();
+      }
+    });
+  }
 
-const getMostRecentEdit = (message) => {
-  return message.edits.shift()
-}
+  onMessageUpdate = () => {
+    // Find the message edited in announcements and replace the content
+    this.bot.on('messageUpdate', (om, nm) => {
+      if (nm.channel.id === CHANNEL_ID) {
+        let m = announcements.find(m => m.id === om.id);
+        m.message = nm.content;
+        this.ss.emit();
+      }
+    });
+  }
 
-const commonMessageFilter = () => {
-  return (oldMessage, newMessage) => {
-    return oldMessage.id === newMessage.id;
+  onMessageDelete = () => {
+    // Find the message deleted in announcements and remove it
+    this.bot.on('messageDelete', (m) => {
+      if (m.channel.id === CHANNEL_ID) {
+        let index = announcements.findIndex(a => a.id === m.id);
+        announcements.splice(index, 1);
+        this.ss.emit();
+      }
+    });
+  }
+
+  login = () => {
+    this.bot.login(BOT_TOKEN);
   }
 }
 
-const findByMessage = (list, time) => {
-  let index = list.map( (element) => {
-    return element.message;
-  }).indexOf(time);
-  return list[index];
+class SocketServer {
+  constructor() {
+    const server = require('express')()
+      .use((req, res) => res.sendFile('index.html', { root: __dirname }))
+      .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+    const io = require('socket.io')(server, {
+      path: '/announcements',
+      serveClient: false,
+    });
+
+    this.io = io;
+  }
+
+  onConnection = () => {
+    // When a user connects to the server, open a socket and emit announcements.
+    this.io.on('connection', (socket) => {
+      console.log(`Socket ${socket.id} connected.`);
+      socket.emit('announcements', announcements);
+      
+      socket.on('disconnect', () => {
+        console.log(`Socket ${socket.id} disconnected.`);
+      });
+    });
+  }
+
+  emit = () => {
+    this.io.emit('announcements', announcements);
+  }
 }
 
-bot.on('ready', (event) => {
-  console.log('Logged in as %s - %s\n', bot.user.tag, bot.user.id);
-  channel = bot.channels.get(CHANNEL_ID);
+let ss = new SocketServer();
+let dbot = new DiscordBot(ss);
 
-  // Get Announcements
-  channel.fetchMessages({ limit: 20 })
-  .then(messages => {
-    messages = messages.filter(deletedMessageFilter());
-    announcements = messages.map(m => ({
-      "id": m.id,
-      "time": m.createdAt,
-      "message": (isEdited(m) ? getMostRecentEdit(m) : m.content)
-    })).reverse();
-  }).catch(console.error);
-
-  io.on('connection', (socket) => {
-    console.log(`Socket ${socket.id} connected.`);
-    socket.emit('announcements', announcements);
-    
-    socket.on('disconnect', () => {
-      console.log(`Socket ${socket.id} disconnected.`);
-      //socket.connect();
-    });
-  });
-
-  bot.on('message', m => {
-    // When a new message is created, add it to the announcements
-    if (m.channel.id === CHANNEL_ID) {
-      announcements.push({
-        "id": m.id,
-        "time": m.createdAt, 
-        "message": m.content
-      });
-      io.emit('announcements', announcements);
-    }
-  });
-
-  bot.on('messageUpdate', (om, nm) => {
-    // Find the message edited in announcements and replace the content
-    if (nm.channel.id === CHANNEL_ID) {
-      let m = announcements.find(m => m.id === om.id);
-      m.message = nm.content;
-      io.emit('announcements', announcements);
-    }
-  });
-
-  bot.on('messageDelete', (m) => {
-    // Find the message deleted in announcements and remove it
-    if (m.channel.id === CHANNEL_ID) {
-      let index = announcements.findIndex(a => a.id === m.id);
-      announcements.splice(index, 1);
-      io.emit('announcements', announcements);
-    }
-  });
-});
-
-bot.login(BOT_TOKEN)
+dbot.onReady();
+dbot.login();
