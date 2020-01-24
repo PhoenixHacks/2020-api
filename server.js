4000f8a112da445e8b376f6de32c6b5b9eaa6594
Justin McGettigan <jwmcgettigan@gmail.com>
2020-01-24 18:33:08 -0500

    Attempting to fix Firefox CORS issue (2)

--- server.js
+++ server.js
@@ -95,15 +95,7 @@ class SocketServer {
     const io = require('socket.io')(server, {
       path: '/announcements',
       serveClient: false,
-      handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": "https://live.phoenixhacks.com",
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-      }
+      origins: '*:*'
     });
 
     this.io = io;
